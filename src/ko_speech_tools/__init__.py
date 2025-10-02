75408d16f18e67ca31fe4421cffab691d29997f3
Enno Hermann <enno.hermann@gmail.com>
2025-10-02 21:51:47 +0200

    feat: copy g2p code
    Source: https://github.com/harmlessman/g2pkk

--- src/ko_speech_tools/__init__.py
+++ src/ko_speech_tools/__init__.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from ko_speech_tools.g2p.g2p import G2p
 from ko_speech_tools.romanize import hangul_romanize
 
-__all__ = ["hangul_romanize"]
+__all__ = ["G2p", "hangul_romanize"]
